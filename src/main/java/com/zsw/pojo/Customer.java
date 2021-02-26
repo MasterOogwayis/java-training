8543bff0ef2ae9c377a35f68f4caac0c88dbe8a6
zsw <499504777@qq.com>
2021-02-26 15:25:29 +0800

    update

---
+++ src/main/java/com/zsw/pojo/Customer.java
@@ -0,0 +1,7 @@
+package com.zsw.pojo;
+
+/**
+ * @author ZhangShaowei on 2021/2/26 15:23
+ */
+public record Customer(String name, int age, String address) {
+}
---
+++ src/main/java/com/zsw/pojo/StaticTests.java
@@ -0,0 +1,12 @@
+package com.zsw.pojo;
+
+/**
+ * @author ZhangShaowei on 2021/2/26 15:24
+ */
+public class StaticTests {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer("zsw", 18, "Mars");
+    }
+
+}
